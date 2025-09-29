cd4b9d5d9bf6443ddd2da20b013e1e50998264f2
vkmbits <vaisakhkmaccformusic@gmail.com>
2025-09-29 20:29:16 +0530

    2025

---
+++ sem7-2025-consolidated/script.sh
@@ -0,0 +1,10 @@
+# pdfjam --nup 2x5 --paper a4paper --outfile 2x5/infosec2x5.pdf infosec.pdf
+# pdfjam --nup 2x5 --paper a4paper --outfile 2x5/cn2x5.pdf CN-saiteja.pdf              
+# pdfjam --nup 2x5 --paper a4paper --outfile 2x5/se2x5.pdf SE\ merged\ version.pdf
+# pdfjam --nup 2x5 --paper a4paper --outfile 2x5/hci2x5.pdf ./hci.pdf
+# pdfjam --landscape --nup 3x3 --paper a4paper --outfile 3x3/hci3x3.pdf ./hci.pdf
+# pdfjam --landscape --nup 2x2 --paper a4paper --outfile 2x2/hci2x2.pdf ./hci.pdf
+
+pdfjam --nup 2x5 --paper a4paper --outfile 2x5/se2x5.pdf ./se.pdf
+pdfjam --landscape --nup 3x3 --paper a4paper --outfile 3x3/se3x3.pdf ./se.pdf
+pdfjam --landscape --nup 2x2 --paper a4paper --outfile 2x2/se2x2.pdf ./se.pdf
