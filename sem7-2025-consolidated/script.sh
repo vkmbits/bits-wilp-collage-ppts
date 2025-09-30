b6c5633ae677c1f236b4b6eb7ec17839977bfe2d
vkmbits <vaisakhkmaccformusic@gmail.com>
2025-09-30 19:39:34 +0530

    2025 index numbered

--- sem7-2025-consolidated/script.sh
+++ sem7-2025-consolidated/script.sh
@@ -5,6 +5,22 @@
 # pdfjam --landscape --nup 3x3 --paper a4paper --outfile 3x3/hci3x3.pdf ./hci.pdf
 # pdfjam --landscape --nup 2x2 --paper a4paper --outfile 2x2/hci2x2.pdf ./hci.pdf
 
-pdfjam --nup 2x5 --paper a4paper --outfile 2x5/se2x5.pdf ./se.pdf
-pdfjam --landscape --nup 3x3 --paper a4paper --outfile 3x3/se3x3.pdf ./se.pdf
-pdfjam --landscape --nup 2x2 --paper a4paper --outfile 2x2/se2x2.pdf ./se.pdf
+# pdfjam --nup 2x5 --paper a4paper --outfile 2x5/se2x5.pdf ./se.pdf
+# pdfjam --landscape --nup 3x3 --paper a4paper --outfile 3x3/se3x3.pdf ./se.pdf
+# pdfjam --landscape --nup 2x2 --paper a4paper --outfile 2x2/se2x2.pdf ./se.pdf
+
+#--- numbering
+
+# Loop through every PDF in current directory and subdirectories
+
+find . -type f -name "*.pdf" | while read -r file; do
+    dir=$(dirname "$file")
+    base=$(basename "$file" .pdf)
+
+    # Backup original
+    backup="$dir/${base}-nonnumbered.pdf"
+    mv "$file" "$backup"
+
+    echo "Stamping $backup -> $file"
+    pdfcpu stamp add -m text -- "%p" "pos:br, scale:0.02, rot:0" "$backup" "$file"
+done
